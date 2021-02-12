189a2b7efafc9a14f160e406882073289c0514d8
raquel.paternazi <raquel.paternazi@BR-IT00182.globant.com>
2021-02-12 20:29:44 -0300

    Gente, segue as alterações, por favor confiram!!!

--- AC1/src/Arranjo.java
+++ AC1/src/Arranjo.java
@@ -1,24 +1,9 @@
 public class Arranjo {
-
-	public static void main(String args[]) {
-		// TODO Auto-generated method stub
-		int[] a = {940, 880, 830, 790, 750, 660, 650, 590, 510, 940};
-	}
-
 	
-	public Integer menor() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public Integer maior() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
+	int[] a = {940, 880, 830, 790, 750, 660, 650, 590, 510, 940};
+		
 	public int soma() {
 		int soma = 0;
-		int[] a = {940, 880, 830, 790, 750, 660, 650, 590, 510, 940};
 		int i;
 
 		for(i=0; i < a.length; i++) {
@@ -27,15 +12,41 @@ public class Arranjo {
 		
 		return soma;
 	}
-
+	
+	public Integer maior() {
+		int maior = 0;
+		
+		for(int i = 0; i <a.length; i++) {
+			if(a[i] > maior) {
+				maior = a[i];
+			}
+		}
+		return maior;
+	}	
+	
+	public Integer menor() {
+		int menor = 0;
+		
+		for(int i = 0; i <a.length; i++) {
+			if(i==0) {
+				menor=a[i];
+			}else {
+				if(a[i]<menor) {
+					menor = a[i];
+				}
+			}
+		}		
+		return menor;
+	}
+	
 	public Integer repeticoes(int item) {
-		int[] a = {940, 880, 830, 790, 750, 660, 650, 590, 510, 940}
 		int contador = 0;
 		for(int i = 0; i < a.length; i++) {
 			if(a[i] == item) {
-				contador += 1
+				contador += 1;
 			}
 		}
 		return contador;
 	}
 }
+
